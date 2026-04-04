d488f20157c030e46085e9f51003a01cc938c589
Mohamed-Mos-aad <ma3268787@gmail.com>
2026-04-04 13:08:27 +0200

    Design updated

---
+++ src/app/notifications/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+    return (
+        <div>Soon</div>
+    );
+}
--- src/components/navigation/SideNavBar.tsx
+++ src/components/navigation/SideNavBar.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+
 // ** Components
 import {
     Sidebar,
@@ -11,18 +14,20 @@ import {
 } from "@/components/ui/sidebar"
 import { AddTodoDialog } from "@/components/AddTodoDialog";
 import { LayoutDashboard, CheckSquare, Bell } from "lucide-react";
+import Link from "next/link";
+import {usePathname} from "next/navigation";
 
 export function SideNavBar() {
+    const pathname = usePathname();
+    const isLinkActive = (path: string) => pathname === path;
+
     return (
-        // استخدمنا border-border بدلاً من slate-200
         <Sidebar className="border-r border-border bg-sidebar">
             <SidebarHeader className="p-6">
                 <div className="flex items-center gap-3">
-                    {/* Logo Placeholder */}
                     <div className="size-8 rounded-lg bg-primary flex items-center justify-center text-primary-foreground font-bold shadow-sm">
                         T
                     </div>
-                    {/* text-foreground عشان يقلب أبيض في الدارك مود */}
                     <h1 className="text-xl font-bold tracking-tight text-foreground">
                         TaskFlow
                     </h1>
@@ -32,30 +37,41 @@ export function SideNavBar() {
             <SidebarContent className="px-4">
                 <SidebarGroup>
                     <SidebarMenu className="gap-2">
-                        <SidebarMenuItem>
-                            <SidebarMenuButton
-                                isActive
-                                className="w-full justify-start gap-3 px-4 py-6 text-base font-medium transition-all data-[active=true]:bg-primary/10 data-[active=true]:text-primary"
-                            >
-                                <LayoutDashboard className="size-5" />
-                                <span>Dashboard</span>
-                            </SidebarMenuButton>
-                        </SidebarMenuItem>
+                        <Link href="/">
+                            <SidebarMenuItem>
+                                <SidebarMenuButton
+                                    isActive={isLinkActive("/")}
+                                    className="w-full justify-start gap-3 px-4 py-6 text-base font-medium transition-all data-[active=true]:bg-primary/10 data-[active=true]:text-primary"
+                                >
+                                    <LayoutDashboard className="size-5" />
+                                    <span>Dashboard</span>
+                                </SidebarMenuButton>
+                            </SidebarMenuItem>
+                        </Link>
 
-                        <SidebarMenuItem>
-                            {/* hover:text-foreground و hover:bg-muted بتخلي التفاعل أنعم في الدارك مود */}
-                            <SidebarMenuButton className="w-full justify-start gap-3 px-4 py-6 text-base font-medium text-muted-foreground hover:text-foreground hover:bg-muted/50 transition-all">
-                                <CheckSquare className="size-5" />
-                                <span>My Tasks</span>
-                            </SidebarMenuButton>
-                        </SidebarMenuItem>
+                        <Link href="/tasks">
+                            <SidebarMenuItem>
+                                <SidebarMenuButton
+                                    isActive={isLinkActive("/tasks")}
+                                    className="w-full justify-start gap-3 px-4 py-6 text-base font-medium transition-all data-[active=true]:bg-primary/10 data-[active=true]:text-primary"
+                                    >
+                                    <CheckSquare className="size-5" />
+                                    <span>My Tasks</span>
+                                </SidebarMenuButton>
+                            </SidebarMenuItem>
+                        </Link>
 
-                        <SidebarMenuItem>
-                            <SidebarMenuButton className="w-full justify-start gap-3 px-4 py-6 text-base font-medium text-muted-foreground hover:text-foreground hover:bg-muted/50 transition-all">
-                                <Bell className="size-5" />
-                                <span>Notifications</span>
-                            </SidebarMenuButton>
-                        </SidebarMenuItem>
+                        <Link href="/notifications">
+                            <SidebarMenuItem>
+                                <SidebarMenuButton
+                                    isActive={isLinkActive("/notifications")}
+                                    className="w-full justify-start gap-3 px-4 py-6 text-base font-medium transition-all data-[active=true]:bg-primary/10 data-[active=true]:text-primary"
+                                    >
+                                    <Bell className="size-5" />
+                                    <span>Notifications</span>
+                                </SidebarMenuButton>
+                            </SidebarMenuItem>
+                        </Link>
                     </SidebarMenu>
                 </SidebarGroup>
             </SidebarContent>
